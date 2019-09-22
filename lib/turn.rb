4e4d7562dd25b585c250f88764ccc7e8127e5bc6
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-22 17:36:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,10 @@ def input_to_index(input)
 end
 
 def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O" || board[index] == "x" || board[index] == "o"
+    true
 end
 
 def valid_move?(board, index)
