e696b78f0d68c5c3b50c8e8928c21377837da152
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-22 18:12:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,4 +31,10 @@ def move(board, index, tag = "X")
 end
 
 def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index)
+  end
 end
