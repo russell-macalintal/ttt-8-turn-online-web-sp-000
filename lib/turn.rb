5748bf79e6fb9b72ee3da0ede6725c81344d954d
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-22 16:57:53 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+end
+
+def position_taken?(board, index)
+end
+
+def valid_move?(board, index)
+end
+
+def move(board, index)
+end
